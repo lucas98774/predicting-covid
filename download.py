63b8cb0870d6ced3a35bf94d6726eef4edf08feb
lucas <lucasspellman98@gmail.com>
2022-02-08 21:27:49 -0600

    wrap the download process in a function

--- download.py
+++ download.py
@@ -3,7 +3,7 @@ from sodapy import Socrata
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
-client = Socrata("data.virginia.gov", None)
+# client = Socrata("data.virginia.gov", None)
 
 # Example authenticated client (needed for non-public datasets):
 # client = Socrata(data.virginia.gov,
@@ -13,7 +13,42 @@ client = Socrata("data.virginia.gov", None)
 
 # Returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("bre9-aqqr")
+# results = client.get("bre9-aqqr")
 
 # Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
+# results_df = pd.DataFrame.from_records(results)
+
+def download_data():
+    """
+    Function to download public dataset on covid numbers ...
+
+    # NOTE: is there any parameters that allow you to modify what is downloaded??? --- dates or anything else?
+    Parameters
+    ----------
+    None
+
+
+    Returns
+    -------
+    results_df: pd.DataFrame
+        The dataframe containing info on the covid cases and deaths ...
+    """
+
+    # Unauthenticated client only works with public data sets. Note 'None'
+    # in place of application token, and no username or password:
+    client = Socrata("data.virginia.gov", None)
+
+    # Example authenticated client (needed for non-public datasets):
+    # client = Socrata(data.virginia.gov,
+    #                  MyAppToken,
+    #                  userame="user@example.com",
+    #                  password="AFakePassword")
+
+    # Returned as JSON from API / converted to Python list of
+    # dictionaries by sodapy.
+    results = client.get("bre9-aqqr")
+
+    # Convert to pandas DataFrame
+    results_df = pd.DataFrame.from_records(results)
+
+    return results_df
